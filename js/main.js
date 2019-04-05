1b97f9fd00bb03986c379e9959c31175caeb1d50
imdangodaane <imdangodaane@gmail.com>
2019-04-05 14:32:18 +0700

    asd

--- js/main.js
+++ js/main.js
@@ -1,53 +1,79 @@
-$(document).ready(function () {
-    // Fetch list of photos here and use Jquery to render it dynamically.
-   mHeritageGoService.getPhotos().then(photos => {
-      for(let i in photos){
+function fetchData(options) {
+    return mHeritageGoService.getPhotos(options);
+}
+
+function renderPhotos(options, photos){
+    for (let i in photos) {
         let photoId = photos[i];
+
+        let idSection = "section-" + (options.offset + parseInt(i));
+        $("#sample_section").clone().prop({
+            "hidden": false,
+            "id": idSection
+        }).appendTo(["#main-content", "#main-content-blurred"]);
         mHeritageGoService.getPhoto(photoId).then(res => {
-          console.log(res);
-          let idSection = "section-" + i;
-          $("#main-content").append('<section class="section" id="'+idSection+'">');
-          $("#"+idSection).append('<aside class="section__aside-left"></aside>');
-          $("#"+idSection).append(' <div class="post">\n' +
-                  '        <div class="post__title">\n' +
-                  '          <img class="post__title__avatar" src="http:'+res.account.picture_url+'" />\n' +
-                  '          <div class="post__title__information">\n' +
-                  '            <div class="post__title__information__caption">\n' +
-                  '              <span style="font-weight: 500; font-size: 18px">'+ res.title[0].content + '</span>\n' +
-                  '              <i class="fas fa-language fa-lg post__title__information__translate-icon"></i>\n' +
-                  '            </div>\n' +
-                  '            <div class="post__title__information__location">\n' +
-                  '              <i class="fas fa-map-marker-alt"></i><span style="margin-left: 5px;">' + res.area_name + '</span>\n' +
-                  '              <i class="far fa-clock" style="margin-left: 20px;"></i><span style="margin-left: 5px;">' + parseInt(res.creation_time) + '</span>\n' +
-                  '            </div>\n' +
-                  '          </div>\n' +
-                  '        </div>' +
-                  '<div class="post__image">\n' +
-                  '          <img src="http:'+ res.image_url +'?size=large" />\n' +
-                  '        </div>\n' +
-                  '        <div class="post__interact">\n' +
-                  '          <div class="post__interact__user-react">\n' +
-                  '            <i class="fas fa-heart fa-lg"></i><strong style="margin-left: 5px;">0</strong>\n' +
-                  '            <i class="fas fa-comment fa-lg" style="margin-left: 30px;"></i></i><strong style="margin-left: 5px;">0</strong>\n' +
-                  '            <i class="fas fa-camera fa-lg" style="margin-left: 30px;"></i><strong style="margin-left: 5px;">0</strong>\n' +
-                  '          </div>\n' +
-                  '          <i class="fas fa-bookmark fa-lg" style="margin-right: 1rem; margin-bottom: 1rem;"></i>\n' +
-                  '        </div>\n' +
-                  '        <!-- <hr class="mb-4"> -->\n' +
-                  '        <div class="post__comment">\n' +
-                  '          <div class="input-wrapper post__comment__input">\n' +
-                  '            <input type="text" placeholder=" Write a comment">\n' +
-                  '          </div>\n' +
-                  '          <i class="fab fa-telegram-plane" style="margin-right: 30px; color: rgba(0, 0, 0, 0.3);"></i>\n' +
-                  '        </div>\n' +
-                  '      </div>');
-          $("#"+idSection).append('<aside class="section__aside-right"></aside>');
+            let titlePart = "#" + idSection + " > .post > .post__title";
+            let imagePart = "#" + idSection + " > .post > .post__image";
+
+            $(titlePart + " > .post__title__avatar").prop("src", "http:" + res.account.picture_url);
+            $(titlePart + " > .post__title__information > .post__title__information__caption > span").text(res.title[0].content);
+            $(titlePart + " > .post__title__information > .post__title__information__location > #location").text(res.area_name);
+            $(titlePart + " > .post__title__information > .post__title__information__location > #date").text(parseInt(res.creation_time));
+
+            $(imagePart + " > img").prop("src", "http:" + res.image_url + "?size=large");
+
         }).catch(error => {
             console.log(error);
-        })
-      }
-    }).catch(error => {
-        console.log(error);
-        alert("There are errors. Try again!")
-   })
+        });
+    }
+}
+
+
+function canLoadMore(options){
+    let one = document.getElementById("section-" + (options.offset));
+    let two = document.getElementById("section-" + (options.offset + 1));
+    if(one && two){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function blurHeader() {
+  var content = $('#main-content'), header = $('#blur-header');
+  // $(content).clone().prop("id", "main-content-blurred").prependTo(header).addClass("blurred");
+  // $(header).addClass("blurred");
+  // $(header).children().replaceWith($(content).children().clone());
+  // $(content).children().clone().appendTo(header);
+  // $('#main-content-blurred').replaceWith($(content).clone().prop("id", "main-content-blurred").addClass("blurred"));
+  // $(content).clone().prependTo(header).addClass('blurred');
+  $('#main-content-blurred').addClass('blurred');
+}
+
+$(document).ready(function () {
+    // Fetch list of photos here and use Jquery to render it dynamically.
+    let loadFinish = true;
+    let options = {offset: 0, limit: 2};
+    let api = fetchData(options);
+    api.then(res => {
+        renderPhotos(options, res);
+    });
+    $(window).scroll(function () {
+        if($(window).scrollTop() == $(document).height() - $(window).height() && $(window).scrollTop() != 0 && canLoadMore(options)){
+            options.offset += options.limit;
+            api = fetchData(options);
+            api.then(res => {
+                renderPhotos(options, res);
+            })
+        }
+    });
+    // Blur header
+    blurHeader();
+    $(document).scroll(function(){
+      var scroll = $(this).scrollTop();
+      $('.blurred').css({
+        '-webkit-transform' : 'translateY(-'+scroll+'px)',
+        'transform' : 'translateY(-'+scroll+'px)'
+      });
+    })
 });
